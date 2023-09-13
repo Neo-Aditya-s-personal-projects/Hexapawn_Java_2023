d32e06b51870e0474414694cd4d159abf31612b2
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-09-13 14:19:57 -0400

    added stuff

---
+++ SideChooser.java
@@ -0,0 +1,15 @@
+import java.utils.Scanner;
+
+public class SideChooser
+{
+    private boolean isFirst = true;
+    public static void Run() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to go First or Second?")
+        String sideChosen = input.nextLine();
+        while (!(sideChosen.equals("First")) && !(sideChosen.equals("Second"))) {
+            System.out.println("Invalid response, would you like to go First or Second?")
+        }
+        isFirst = sideChosen.equals("First");
+    }
+}

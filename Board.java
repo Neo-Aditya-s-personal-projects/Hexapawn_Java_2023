3513133e0eae9a9c4bf808a4ab248ce121018f8f
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-10-03 10:49:57 -0400

    I'm gonna commit this before I lose this

--- Board.java
+++ Board.java
@@ -1,6 +1,6 @@
 public class Board
 {
-    private char[] positions = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
+    private String[] positions = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
     private char aiChar;
     private char playerChar;
     public static void SetupBoard(char aiChar, char playerChar) {
@@ -17,11 +17,40 @@ public class Board
         boolean isValid = true;
         if (isPlayer) {
         isValid = (((finalposition - initialPosition) <= 6) && (finalposition > initialPosition));
-        boolean isValidPiece = ((positions[initialPosition]) && !(position[initialPosition].equals(position[finalposition])));
-        boolean isValidMovement = false;
-        if ((initialPosition % 2) == (finalposition % 2)) {
-            
-        }
+        boolean isValidPiece = ((positions[initialPosition].equals(playerChar)) && !(position[initialPosition].equals(position[finalposition])));
+        boolean isValidMovement = isValidPiece && ValidMovement(initialPosition, finalPosition);
         }
     }
+
+    private static boolean ValidMovement(int initialPosition, int finalposition) {
+        if(positions[finalPosition].equals(aiChar)) {
+            return ValidCapture(initialPosition, finalPosition)
+        }
+        else if(positions[finalPosition].equals(playerChar)) {
+            return false;
+        }
+        else{
+            return (finalPosition == (initialPosition + 3));
+        }
+    }
+    private static boolean ValidCapture(int initialPosition, int finalposition)(
+        if((positions[initialPosition].equals(aiChar)) || (positions[finalPosition].equals(playerChar))) {
+            return false;
+        }
+        if (finalPosition == 4) {
+            return ((initialPosition == 0) || (initialPosition == 2));
+        }
+        else if (initialPosition == 1) {
+            return((finalPosition == 3) ||(finalPosition == 5));
+        }
+        else if(finalPosition == 7 ) {
+            return((initialPosition == 3) || (initialPosition == 5));
+        }
+        else if(initialPosition == 4) {
+            return((finalPosition == 6) ||(finalPosition == 8));
+        }
+        else {
+            return false;
+        }
+    )
 }
